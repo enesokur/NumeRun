c2d9709431bb7f3bc3471ccab5f546bb30a79099
Ömer Öztürk <74858398+omerozturkk@users.noreply.github.com>
2022-05-29 18:35:04 +0300

    Add files via upload
    User Input alınıp şıklara tıklama eklendi, can azalması eklendi. Cevabın doğru olup olmamasına göre oyun ilerledi. Can bitince oyun durması eklendi. Diğer sorulara ilerleme eklendi.

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -12,35 +12,57 @@ public class UI : MonoBehaviour
     int counter = 2;
     GameObject myCharacter;
     GameObject roadTwo;
+    GameObject roadOne;
     public GameObject panel;
     bool calculateQuestion;
-    int gameDifficulty = 0; // 0 = Normal, 1 = Hard 
-    int gameMode = 1; // 0 = Mathematical Operations, Choosing Operator
+    int gameDifficulty = 1; // 0 = Normal, 1 = Hard 
+    int gameMode = 0; // 0 = Mathematical Operations, Choosing Operator
     //int gameDifficulty;
     //int gamemode;
     public TextMeshProUGUI firstButton;
     public TextMeshProUGUI secondButton;
     public TextMeshProUGUI thirdButton;
     public TextMeshProUGUI fourthButton;
-
+    public GameObject heartOne;
+    public GameObject heartTwo;
+    public GameObject heartThree;
+    public Vector3 touchPosition;
     int randomNumberOne;
     int randomNumberTwo;
     int randomOperator;
+    int trueAnswer;
+    int playerLife;
+    bool detectAnswer;
+    bool clicked;
     void Start(){
         hearts = new GameObject[3];
+        playerLife = 3;
         myCharacter = GameObject.Find("Character");
         roadTwo = GameObject.Find("Road 2");
+        roadOne = GameObject.Find("Road 1");
         calculateQuestion = true;
+        heartOne = GameObject.Find("heart1");
+        heartTwo = GameObject.Find("heart2");
+        heartThree = GameObject.Find("heart3");
+        clicked = false;
     }
 
     void Update(){
         temp += 2 * Time.deltaTime;
         score.text = ((int)temp).ToString();
+        if(myCharacter.transform.position.z - roadOne.transform.position.z >= 25 && calculateQuestion == false){
+            calculateQuestion = true;
+        }
         if(myCharacter.transform.position.z - roadTwo.transform.position.z >= 25 && calculateQuestion){
             Time.timeScale = 0f;
             CreateQuestion();
             panel.SetActive(true);
             calculateQuestion = false;
+            detectAnswer = true;
+            clicked = false;
+        }
+        if(detectAnswer && clicked == false){
+            DetectTouch();
         }
     }
 
@@ -48,6 +70,7 @@ public class UI : MonoBehaviour
         int answer;
         question.text = "";
         randomOperator = Random.Range(1,5);
+        trueAnswer = Random.Range(1,5);
         string[] operatorAnswer = new string[]{">","<","="};
         if (gameMode == 0){
             if(gameDifficulty == 0){
@@ -56,7 +79,7 @@ public class UI : MonoBehaviour
                     randomNumberTwo = Random.Range(10,50);
                     question.text = randomNumberOne + " + " + randomNumberTwo + " = " + " ?";
                     answer = randomNumberOne + randomNumberTwo;
-                    int trueAnswer = Random.Range(1,5);
+
                     if(trueAnswer == 1){
                         firstButton.text = (answer).ToString();
                         secondButton.text = (answer + Random.Range(1,6)).ToString();
@@ -93,7 +116,6 @@ public class UI : MonoBehaviour
                         question.text = randomNumberTwo + " - " + randomNumberOne + " = " + " ?";
                         answer = randomNumberTwo - randomNumberOne;
                     }
-                    int trueAnswer = Random.Range(1,5);
                     if(trueAnswer == 1){
                         firstButton.text = (answer).ToString();
                         secondButton.text = (answer + Random.Range(1,6)).ToString();
@@ -124,7 +146,6 @@ public class UI : MonoBehaviour
                     randomNumberTwo = Random.Range(10,15);
                     question.text = randomNumberOne + " x " + randomNumberTwo + " = " + " ?";
                     answer = randomNumberOne * randomNumberTwo;
-                    int trueAnswer = Random.Range(1,5);
                     if(trueAnswer == 1){
                         firstButton.text = (answer).ToString();
                         secondButton.text = (answer + Random.Range(1,6)).ToString();
@@ -155,7 +176,6 @@ public class UI : MonoBehaviour
                     randomNumberOne = Random.Range(1,7)*randomNumberTwo;
                     question.text = randomNumberOne + " / " + randomNumberTwo + " = " + " ?";
                     answer = randomNumberOne / randomNumberTwo;
-                    int trueAnswer = Random.Range(1,5);
                     if(trueAnswer == 1){
                         firstButton.text = (answer).ToString();
                         secondButton.text = (answer + Random.Range(1,6)).ToString();
@@ -188,7 +208,6 @@ public class UI : MonoBehaviour
                     randomNumberTwo = Random.Range(30,99);
                     question.text = randomNumberOne + " + " + randomNumberTwo + " = " + " ?";
                     answer = randomNumberOne + randomNumberTwo;
-                    int trueAnswer = Random.Range(1,5);
                     if(trueAnswer == 1){
                         firstButton.text = (answer).ToString();
                         secondButton.text = (answer + Random.Range(1,6)).ToString();
@@ -225,7 +244,6 @@ public class UI : MonoBehaviour
                         question.text = randomNumberTwo + " - " + randomNumberOne + " = " + " ?";
                         answer = randomNumberTwo - randomNumberOne;
                     }
-                    int trueAnswer = Random.Range(1,5);
                     if(trueAnswer == 1){
                         firstButton.text = (answer).ToString();
                         secondButton.text = (answer + Random.Range(1,6)).ToString();
@@ -256,7 +274,6 @@ public class UI : MonoBehaviour
                     randomNumberTwo = Random.Range(1,20);
                     question.text = randomNumberOne + " x " + randomNumberTwo + " = " + " ?";
                     answer = randomNumberOne * randomNumberTwo;
-                    int trueAnswer = Random.Range(1,5);
                     if(trueAnswer == 1){
                         firstButton.text = (answer).ToString();
                         secondButton.text = (answer + Random.Range(1,6)).ToString();
@@ -287,7 +304,6 @@ public class UI : MonoBehaviour
                     randomNumberOne = Random.Range(1,10)*randomNumberTwo;
                     question.text = randomNumberOne + " / " + randomNumberTwo + " = " + " ?";
                     answer = randomNumberOne / randomNumberTwo;
-                    int trueAnswer = Random.Range(1,5);
                     if(trueAnswer == 1){
                         firstButton.text = (answer).ToString();
                         secondButton.text = (answer + Random.Range(1,6)).ToString();
@@ -316,7 +332,7 @@ public class UI : MonoBehaviour
             }   
         }
         else if (gameMode == 1){
-            int trueAnswer = Random.Range(1,4);
+            trueAnswer = Random.Range(1,4);
             if(gameDifficulty == 0){
                 randomNumberOne = Random.Range(10,200);
                 randomNumberTwo = Random.Range(10,200);
@@ -377,9 +393,127 @@ public class UI : MonoBehaviour
                         thirdButton.text = operatorAnswer[2].ToString();
                     }
                 }
-            }
-        }    
+        }
+        if (gameMode == 0){
+            switch (trueAnswer){
+            case 1:
+                print ("True Answer is first option.");
+                break;
+            case 2:
+                print ("True Answer is second option.");
+                break;
+            case 3:
+                print ("True Answer is third option.");
+                break;
+            case 4:
+                print ("True Answer is fourth option.");
+                break;
+            default:
+                print ("An error has occured.");
+                break;
+            }      
+        }
+        if (gameMode == 1){
+            switch (trueAnswer){
+            case 1:
+                print ("True Answer is first option.");
+                break;
+            case 2:
+                print ("True Answer is second option.");
+                break;
+            case 3:
+                print ("True Answer is third option.");
+                break;
+            default:
+                print ("An error has occured.");
+                break;
+            }      
+        }       
     }
+
+    void DetectTouch(){   
+        clicked = false;
+        if(Input.touchCount == 1){
+                Touch touch = Input.GetTouch(0); // get the touch
+            if(touch.phase == TouchPhase.Began){
+                if(touch.position.x <= 350 && touch.position.x >= 100 && touch.position.y <= 1900 && touch.position.y >= 1650){
+                    if(trueAnswer == 1){
+                        print("Doğru cevap verdiniz, tebrikler.");
+                        //detectAnswer = false;
+                    }
+                    else{
+                        print("Cevap" + trueAnswer.ToString() + " . şık olmalıydı. Yanlış bildiniz.");
+                        playerLife = playerLife -1;
+                        print(playerLife.ToString() + " canınız kaldı.");
+                    }
+                    clicked = true;      
+                }
+                else if(touch.position.x <= 650 && touch.position.x >= 450 && touch.position.y <= 1900 && touch.position.y >= 1650){
+                    print("İkinci şık tıklandı.");
+                    if(trueAnswer == 2){
+                        print("Doğru cevap verdiniz, tebrikler.");
+                    }
+                    else{
+                        print("Cevap" + trueAnswer.ToString() + " . şık olmalıydı. Yanlış bildiniz.");
+                        playerLife = playerLife -1;
+                        print(playerLife.ToString() + " canınız kaldı.");
+                    }
+                clicked = true;    
+                }
+                else if(touch.position.x <= 990 && touch.position.x >= 740 && touch.position.y <= 1900 && touch.position.y >= 1650){
+                    if(trueAnswer == 3){
+                        print("Doğru cevap verdiniz, tebrikler.");
+                    }
+                    else{
+                        print("Cevap" + trueAnswer.ToString() + " . şık olmalıydı. Yanlış bildiniz.");
+                        playerLife = playerLife -1;
+                        print(playerLife.ToString() + " canınız kaldı.");
+                    }
+                clicked = true;    
+                }
+                else if(touch.position.x <= 1300 && touch.position.x >= 1080 && touch.position.y <= 1900 && touch.position.y >= 1650){
+                    if(trueAnswer == 4){
+                        print("Doğru cevap verdiniz, tebrikler.");
+                    }
+                    else{
+                        print("Cevap" + trueAnswer.ToString() + " . şık olmalıydı. Yanlış bildiniz.");
+                        playerLife = playerLife -1;
+                        print(playerLife.ToString() + " canınız kaldı.");
+                    }
+                clicked = true;         
+                }
+                else{
+                    print("Tıklanan konum --> " + touch.position.y.ToString());
+                }
+            
+                switch (playerLife){
+                    case 0:
+                        print("Canınız kalmamıştır, oyun sonlandı.");
+                        heartOne.SetActive(false);
+                         // Game over eklenecek.
+                        break;
+                    case 1:
+                        heartTwo.SetActive(false);
+                        break;
+                    case 2:                     
+                        heartThree.SetActive(false);                
+                        break;
+                    default:
+                        break;
+                }      
+            }         
+            if(clicked == true){
+                detectAnswer = false;
+                Time.timeScale = 1f;
+                panel.SetActive(false);  
+            if(playerLife == 0){
+                Time.timeScale= 0f;
+            }
+            }               
+        }
+    }
+        
+}
     // Soruyu yanlış bildiğinde counterı bir eksilt ve kalplerden birini sil.
 
 
